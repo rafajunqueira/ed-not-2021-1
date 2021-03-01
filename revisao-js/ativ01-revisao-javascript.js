e1e39faf2b72e5c22b3569a782a938831b1f999c
Rafael <rafa.rjunqueira@gmail.com>
2021-03-01 14:49:57 -0300

    exercicios finalizados

--- revisao-js/ativ01-revisao-javascript.js
+++ revisao-js/ativ01-revisao-javascript.js
@@ -15,93 +15,92 @@
 
 */
 
-const estadosNe = []
+const estadosNe = [];
 
 let estado1 = {
-    nome: 'Sergipe',
-    sigla: 'SE',
-    area: 21915.08,
-    populacao: 2278308
-}
+  nome: "Sergipe",
+  sigla: "SE",
+  area: 21915.08,
+  populacao: 2278308,
+};
 
-estadosNe.push(estado1)
+estadosNe.push(estado1);
 
 /*
    1) Crie objetos para os demais Estados, segundo o modelo acima, e acrescente-os
       ao vetor estadosNe usando push().
 */
 let estado2 = {
-    nome: 'Alagoas',
-    sigla: 'AL',
-    area: 27848.14,
-    populacao: 3322820
-}
+  nome: "Alagoas",
+  sigla: "AL",
+  area: 27848.14,
+  populacao: 3322820,
+};
 
-estadosNe.push(estado2)
+estadosNe.push(estado2);
 
 let estado3 = {
-    nome: 'Rio Grande do Norte',
-    sigla: 'RN',
-    area: 52811.05,
-    populacao: 3479010
-}
+  nome: "Rio Grande do Norte",
+  sigla: "RN",
+  area: 52811.05,
+  populacao: 3479010,
+};
 
-estadosNe.push(estado3)
+estadosNe.push(estado3);
 
 let estado4 = {
-    nome: 'Paraíba',
-    sigla: 'PB',
-    area: 56469.78,
-    populacao: 3996496
-}
+  nome: "Paraíba",
+  sigla: "PB",
+  area: 56469.78,
+  populacao: 3996496,
+};
 
-estadosNe.push(estado4)
+estadosNe.push(estado4);
 
 let estado5 = {
-    nome: 'Pernambuco',
-    sigla: 'PE',
-    area: 98148.32,
-    populacao: 9496294
-}
+  nome: "Pernambuco",
+  sigla: "PE",
+  area: 98148.32,
+  populacao: 9496294,
+};
 
-estadosNe.push(estado5)
+estadosNe.push(estado5);
 
 let estado6 = {
-    nome: 'Ceará',
-    sigla: 'CE',
-    area: 148920.47,
-    populacao: 9075649
-}
+  nome: "Ceará",
+  sigla: "CE",
+  area: 148920.47,
+  populacao: 9075649,
+};
 
-estadosNe.push(estado6)
+estadosNe.push(estado6);
 
 let estado7 = {
-    nome: 'Piauí',
-    sigla: 'PI',
-    area: 251577.74,
-    populacao: 3264531
-}
+  nome: "Piauí",
+  sigla: "PI",
+  area: 251577.74,
+  populacao: 3264531,
+};
 
-estadosNe.push(estado7)
-
+estadosNe.push(estado7);
 
 let estado8 = {
-    nome: 'Maranhão',
-    sigla: 'MA',
-    area: 331937.45,
-    populacao: 7035055
-}
+  nome: "Maranhão",
+  sigla: "MA",
+  area: 331937.45,
+  populacao: 7035055,
+};
 
-estadosNe.push(estado8)
+estadosNe.push(estado8);
 
 let estado9 = {
-    nome: 'Bahia',
-    sigla: 'BA',
-    area: 564733.18,
-    populacao: 14812617
-}
+  nome: "Bahia",
+  sigla: "BA",
+  area: 564733.18,
+  populacao: 14812617,
+};
 
-estadosNe.push(estado9)
+estadosNe.push(estado9);
 
 /*
    2) Escreva uma arrow function que receba dois parâmetros, área e população, e
@@ -109,8 +108,8 @@ estadosNe.push(estado9)
       segunda pela primeira. 
 */
 const densidadeDemografica = (a, p) => {
-    return p / a
-}
+  return p / a;
+};
 
 /*
    3) Percorra o vetor estadosNe usando um for tradicional. Para cada estado,
@@ -123,22 +122,24 @@ const densidadeDemografica = (a, p) => {
 */
 
 for (let i = 0; i < estadosNe.length; i++) {
-    estadosNe[i].densidadeDemografica = densidadeDemografica(estadosNe[i].area, estadosNe[i].populacao)
-    delete estadosNe[i].sigla
+  estadosNe[i].densidadeDemografica = densidadeDemografica(
+    estadosNe[i].area,
+    estadosNe[i].populacao
+  );
+  delete estadosNe[i].sigla;
 }
-
 
 /* 4) Escreva uma arrow function que receba um objeto. Na função, use for..in
       para extrair as propriedades e seus valores e exibi-los com console.log().
 
 */
 exibirObj = (obj) => {
-    for (let prop in obj) {
-        if (prop == 'nome') {
-            console.log(obj[prop])
-        }
+  for (let prop in obj) {
+    if (prop == "nome") {
+      console.log(obj[prop]);
     }
-}
+  }
+};
 
 /* 5) Percorra o vetor estadosNe usando for..of. Para cada objeto no vetor,
       invoque a função escrita em 4) para exibi-lo.
@@ -146,7 +147,7 @@ exibirObj = (obj) => {
 */
 
 for (const est of estadosNe) {
-    exibirObj(est)
+  exibirObj(est);
 }
 
 /* 6)
@@ -161,23 +162,49 @@ for (const est of estadosNe) {
 
 */
 
-const vetorOrdenado = []
+const vetorOrdenado1 = [];
 
+// Ex6 Método 1
+vetorOrdenado1.push(estadosNe[0].nome); // ALAGOAS
+vetorOrdenado1.unshift(estadosNe[1].nome); // SERGIPE
+vetorOrdenado1.splice(1, 0, estadosNe[8].nome); // BAHIA
+vetorOrdenado1.splice(2, 0, estadosNe[5].nome); // CEARÁ
+vetorOrdenado1.splice(3, 0, estadosNe[7].nome); // MARANHÃO
+vetorOrdenado1.splice(4, 0, estadosNe[3].nome); // PARAÍBA
+vetorOrdenado1.splice(5, 0, estadosNe[4].nome); // PERNAMBUCO
+vetorOrdenado1.splice(6, 0, estadosNe[6].nome); // PIAUÍ
+vetorOrdenado1.splice(7, 0, estadosNe[2].nome); // RIO GRANDE DO NORTE
+
+console.log(
+  `Vetor de Estados Ordenado Alfabeticamente Método 1 : ${vetorOrdenado1}`
+);
+
+//
+//* ********************************************************************* */
+//
+
+const vetorOrdenado2 = [];
+
+// Ex6 Método 2
 const addEstado = (estado) => {
-    if (!vetorOrdenado.lenght) {
-        vetorOrdenado.push(estado)
+  if (!vetorOrdenado2.lenght) {
+    vetorOrdenado2.push(estado);
+  }
+  vetorOrdenado2.sort();
+};
+
+const criaNovoObjComPropObjExistente = (obj, propDesejada) => {
+  // CHECA SE VETOR POSSUI PELO MENOS UM OBJETO E SE ESSE PRIMEIRO OBJETO POSSUI O ATRIBUTO PESQUISADO
+  if (hasOwnProperty.call(obj[0], propDesejada)) {
+    // SE VERDADEIRO PARA OS DOIS TESTES ACIMA, ENTÃO APENAS O CONTEÚDO DO ATRIBUTO SELECIONADO É ENVIADO PARA OUTRO VETOR, ATIVANDO O addEstado()
+    for (let i = 0; i < obj.length; i++) {
+      addEstado(obj[i][`${propDesejada}`])
     }
-    vetorOrdenado.sort()
-}
+  }
+};
 
-const addPropObj = (obj) => {
-    for (let prop in obj) {
-        if (obj[prop].nome) {
-            addEstado(obj[prop].nome)
-        }
-    }
-}
+criaNovoObjComPropObjExistente(estadosNe, "nome");
 
-addPropObj(estadosNe)
-
-console.log(`Vetor de Estados Ordenado Alfabeticamente : ${vetorOrdenado}`)
+console.log(
+  `Vetor de Estados Ordenado Alfabeticamente Método 2 : ${vetorOrdenado2}`
+);
